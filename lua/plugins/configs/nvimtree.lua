d7066760f6d6126ca89245e9276695d144d1155a
Matt Weller <hellmitre@gmail.com>
2021-09-07 17:44:40 -0700

    nvimtree config

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -19,12 +19,12 @@ g.nvim_tree_git_hl = 1
 g.nvim_tree_gitignore = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_hijack_netrw = 0
+g.nvim_tree_hijack_netrw = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-g.nvim_tree_side = "left"
+g.nvim_tree_side = "right"
 g.nvim_tree_tab_open = 0
 g.nvim_tree_update_cwd = 1
 g.nvim_tree_width = 25
@@ -63,7 +63,7 @@ g.nvim_tree_icons = {
 }
 
 g.nvim_tree_bindings = {
-   { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb "edit" },
+   { key = { "<CR>", "o", "<2-LeftMouse>", "<Tab>" }, cb = tree_cb "edit" },
    { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb "cd" },
    { key = "<C-v>", cb = tree_cb "vsplit" },
    { key = "<C-x>", cb = tree_cb "split" },
@@ -73,7 +73,7 @@ g.nvim_tree_bindings = {
    { key = "P", cb = tree_cb "parent_node" },
    { key = "<BS>", cb = tree_cb "close_node" },
    { key = "<S-CR>", cb = tree_cb "close_node" },
-   { key = "<Tab>", cb = tree_cb "preview" },
+   --{ key = "<Tab>", cb = tree_cb "preview" },
    { key = "K", cb = tree_cb "first_sibling" },
    { key = "J", cb = tree_cb "last_sibling" },
    { key = "I", cb = tree_cb "toggle_ignored" },
